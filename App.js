17202a2c2d1c9e9e0315251297149f4173784763
Frank Calise <fcalise@gmail.com>
2024-06-09 21:56:52 -0400

    useFonts with react navigation reproducer

--- App.js
+++ App.js
@@ -5,11 +5,24 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { useLoadedAssets } from "./hooks/useLoadedAssets";
 import Navigation from "./navigation";
-import { useColorScheme } from "react-native";
+import { useColorScheme, View } from "react-native";
+
+import { SpaceGrotesk_600SemiBold } from "@expo-google-fonts/space-grotesk";
+import { useFonts } from "expo-font";
 
 export default function App() {
   const isLoadingComplete = useLoadedAssets();
   const colorScheme = useColorScheme();
+
+  let [fontsLoaded, fontError] = useFonts({
+    spaceGroteskBold: SpaceGrotesk_600SemiBold,
+  });
+
+  console.log({ fontError });
+
+  if (!fontsLoaded) {
+    return <View style={{ flex: 1, backgroundColor: "blue" }} />;
+  }
 
   if (!isLoadingComplete) {
     return null;
